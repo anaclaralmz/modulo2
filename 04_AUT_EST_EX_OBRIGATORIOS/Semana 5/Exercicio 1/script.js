46d2e233f04e581ac3e313a23b090a42fd4039b8
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2022-05-23 00:15:11 -0300

    exercicio 1 semana 5

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercicio 1/script.js
@@ -0,0 +1,35 @@
+﻿var calcResDiv = "#calc";
+let numero1 = document.getElementById("numero1")
+let numero2 = document.getElementById("numero2")
+let operação = document.getElementById("operação")
+
+function CalcAddAndShow() {
+    ShowResult(Calc())
+}
+
+function Calc() {
+    let x = numero1.value;
+    let y = numero2.value;
+    let op = operação.value;
+
+    if (op == "+") {
+        result = x + y;
+    } else if (op == "-") {
+        result = x - y;
+    } else if (op == "*") {
+        result = x * y;
+    } else if (op == "/") {
+        result = x / y;
+    } else if (op == "//") {
+        result =  eval(x, op, y);
+    } else if (op == "%") {
+        result = x % y;
+    };
+
+    return result;
+
+}
+ 
+function ShowResult(res) {
+    $(calcResDiv).append(`${numero1.value} ${operação.value} ${numero2.value} = ${res} <br/>`);
+}
