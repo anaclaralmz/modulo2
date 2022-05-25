e1c023fca05ecf7277a36e545db97e33d9b414ac
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2022-05-25 15:14:03 -0300

    exercicio 2 semana 5

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercicio 2/scripts/script.js
@@ -0,0 +1,21 @@
+function calcular(){
+
+    var preço = Number(document.getElementById ("preço").value);
+    var notas = [100, 50, 20, 20, 5, 2, 1];
+    var resultado  = [0,0,0,0,0,0,0]; //armazenar resultados da quantidade de notas
+    var divResultado = document.getElementById("mostrarResultado");
+
+    console.log("Preço:" + preço);
+
+    for (let i = 0; i < notas.length; i++) {
+        console.log("Entrou");
+        if(preço>= notas[i]){
+            var quantidadeNotas = Math.floor(preço/notas[i]);
+            resultado[i] = quantidadeNotas;
+            preço = preço - quantidadeNotas*notas[i];
+            console.log(quantidadeNotas + " notas de " + notas[i]);
+            divResultado.innerHTML += "<br />" + quantidadeNotas + " notas de " + notas[i];
+        }
+    }
+}
+    
