a5469c3ffd8332da25cf1dde05172a184594c492
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2022-05-30 13:53:59 -0300

    update

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercicio 3/scripts/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercicio 3/scripts/script.js
@@ -1,21 +1,12 @@
-let lista = document.getElementById("string");
+let inputList = document.getElementById("string");
+let valorFoco = document.getElementById("valorFoco");
 let listaOrdenada = document.getElementById("resultado");
-function ordenarlista() {
-  let list = lista.value.split(",");
-  list = list.map((item) => parseInt(item));
-  let sortedList = bubblesort(list);
-​
-  let li = document.createElement("li");
-​
-  li.innerHTML = `[${sortedList}]`;
-  listaOrdenada.appendChild(li);
-}
-​
-function bubblesort(list) {
+
+function bubbleSort(list) {
   let i;
   let j;
   let temp;
-​
+
   for (i = 0; i < list.length; i++) {
     for (j = 0; j < list.length - i - 1; j++) {
       if (list[j] > list[j + 1]) {
@@ -27,3 +18,33 @@ function bubblesort(list) {
   }
   return list;
 }
+
+function getvalorFocoIndex(list, value) {
+  let index = 0;
+  let found = false;
+
+  while (index < list.length && !found) {
+    if (list[index] == value) {
+      found = true;
+    } else {
+      index++;
+    }
+  }
+
+  return found ? index : -1;
+}
+
+const sortList = () => {
+  let list = inputList.value.split(",");
+  list = list.map((item) => parseInt(item));
+  let sortedList = bubbleSort(list);
+  let valorFocoIndex = getvalorFocoIndex(sortedList, valorFoco.value);
+
+  let li = document.createElement("li");
+
+  li.innerHTML = `
+  Lista ordenada: [${sortedList}]\n
+  Posição do valor foco: ${valorFocoIndex}
+  `;
+  listaOrdenada.appendChild(li);
+};
